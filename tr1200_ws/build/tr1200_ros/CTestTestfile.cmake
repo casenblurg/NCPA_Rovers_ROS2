0530baa56cad5842ca4ef01fbac92804312c47b5
Your Name <you@example.com>
2025-05-23 12:59:29 -0400

    Test

---
+++ tr1200_ws/build/tr1200_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ncpa/tr1200_ws/tr1200_ros/tr1200_ros
+# Build directory: /home/ncpa/tr1200_ws/build/tr1200_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
