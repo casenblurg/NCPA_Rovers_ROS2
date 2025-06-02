df4eb6d02b3c9284b5c22b0591718e7268bb3c7b
Casey Heustess <casey.heustess7@gmail.com>
2025-06-02 16:46:49 -0400

    Update LT2-3.sh

--- SSH_Into_Rovers/LT2-3.sh
+++ SSH_Into_Rovers/LT2-3.sh
@@ -2,7 +2,7 @@
 
 
 echo -e "Fetching NCPA Rovers IP Address..\n"
-addr=$(sudo arp-scan -I enp0s31f6 -l | grep "enter:the:rovers:ethernet:mac:address" | cut -f1)
+addr=$(sudo arp-scan -I enp0s31f6 -l | grep "enter:Mac" | cut -f1) #only enter the first two sections for examnple 00:00
 
 
 if [ -n "$addr" ]
